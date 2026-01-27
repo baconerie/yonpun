6aa8bcb12ed6c150dd6c0955da8a56ea7daabf9c
baconerie <baconerie0001@gmail.com>
2026-01-27 18:12:07 -0500

    Components mode file

---
+++ src/components/mod.rs
@@ -0,0 +1,29 @@
+/*
+Copyright 2026 baconerie
+
+This file is part of Yonpun.
+
+Yonpun is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Yonpun is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+Yonpun. If not, see <https://www.gnu.org/licenses/>. 
+*/
+
+pub mod divider;
+pub use divider::Divider;
+
+pub mod heading;
+pub use heading::Heading;
+
+pub mod topbar;
+pub use topbar::Topbar;
+
+pub mod dashboard;
+pub use dashboard::Dashboard;
